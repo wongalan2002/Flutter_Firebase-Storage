c26ee44310e2d9723229b10328594fc911ff6f8b
wongalan2002 <wongalan2002@yahoo.com.hk>
2022-01-23 17:53:53 +0800

    Add draw on image

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,116 @@
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'home.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MaterialApp(
+//     home: MyApp(),
+//     debugShowCheckedModeBanner: false,
+//   ));
+// }
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return HomePage();
+//   }
+// }
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_painter/image_painter.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'home.dart';
+import 'dutch_text_delegate.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MaterialApp(
-    home: MyApp(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
+void main() => runApp(App());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      title: 'Image Painter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ImagePainterExample(),
+    );
+  }
+}
+
+class ImagePainterExample extends StatefulWidget {
+  @override
+  _ImagePainterExampleState createState() => _ImagePainterExampleState();
+}
+
+class _ImagePainterExampleState extends State<ImagePainterExample> {
+  final _imageKey = GlobalKey<ImagePainterState>();
+  final _key = GlobalKey<ScaffoldState>();
+
+  void saveImage() async {
+    final image = await _imageKey.currentState!.exportImage();
+    final directory = (await getApplicationDocumentsDirectory()).path;
+    await Directory('$directory/sample').create(recursive: true);
+    final fullPath =
+        '$directory/sample/${DateTime.now().millisecondsSinceEpoch}.png';
+    final imgFile = File('$fullPath');
+    imgFile.writeAsBytesSync(image!);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.grey[700],
+        padding: const EdgeInsets.only(left: 10),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Image Exported successfully.",
+                style: TextStyle(color: Colors.white)),
+            TextButton(
+              onPressed: () => OpenFile.open("$fullPath"),
+              child: Text(
+                "Open",
+                style: TextStyle(
+                  color: Colors.blue[200],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _key,
+      appBar: AppBar(
+        title: const Text("Image Painter Example"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save_alt),
+            onPressed: saveImage,
+          )
+        ],
+      ),
+      body: ImagePainter.asset(
+        "assets/sample.jpg",
+        key: _imageKey,
+        scalable: true,
+        initialStrokeWidth: 2,
+        textDelegate: DutchTextDelegate(),
+        initialColor: Colors.green,
+        initialPaintMode: PaintMode.line,
+      ),
+    );
   }
 }
